e2f372eb2365b01ddb3899b1fdc51976f404fda6
Inzi123 <you@example.com>
2022-04-11 18:54:22 +0200

    commit

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ const navSlide = () => {
     const burger = document.querySelector('.navbar__burger');
     const nav = document.querySelector('.navbar__links');
     const navLinks = document.querySelectorAll('.navbar__links li');
+    const logo = document.querySelector('.navbar__logo');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('navbar__active');
+        logo.classList.toggle('logoopacity');
 
         navLinks.forEach((link, index) =>{
             if(link.style.animation){
                 link.style.animation = '';
             } else {
-                link.style.animation = `navbarLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+                link.style.animation = `navbarLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
+
+        burger.classList.toggle('toggle');
+
     });
+
 
 
 
 };
 
 navSlide();
+
+
+$(window).scroll(function(){
+    if($(window).scrollTop()){
+        $(".navbar").addClass("floatingnav");
+    } else{
+        $(".navbar").removeClass("floatingnav");
+    }
+});
